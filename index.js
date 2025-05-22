320d8398e5217797c4860ec49bf476e81b1d4b0e
juanma-ge <jgomest545@g.educaand.es>
2025-05-22 11:57:02 +0200

    js en principio funcionando

--- index.js
+++ index.js
@@ -5,7 +5,40 @@ document.addEventListener("DOMContentLoaded", () => {
             const parser = new DOMParser();
             const textoXML = parser.parseFromString(xmlTexto, "text/xml");
         })
-    function pasarAXML(){
+    function pasarAXML(xml){
+        const productos = [];
+        const nodo = xml.getElementsByTagName("producto");
 
+        for (let producto of nodo) {
+            const nombre = producto.querySelector(".nombre").textContent;
+            const precio = parseFloat(producto.querySelector(".precio").textContent);
+            const stock = parseFloat(producto.querySelector(".stock").textContent);
+            productos.push(producto, precio, stock);
+        }
+        return productos;
+    }
+
+    function mostrarTabla(inventario){
+        let html =
+            `<table>
+                <thead>
+                <tr><th>Nombre</th><th>Precio</th><th>Stock</th></tr>
+                </thead>
+                <tbody>
+                `;
+
+        inventario.forEach(producto => {
+            const clase = producto.stock < 20 ? "bajoStock" : "";
+            html += `
+              <tr class="${clase}">
+                <td>${producto.nombre}</td>
+                <td>${producto.precio.toFixed(2)} €</td>
+                <td>${producto.stock}</td>
+              </tr>
+            `;
+        })
+
+        html += `</tbody></table>`;
+        document.getElementById("tabla-contenedor").innerHTML = html;
     }
 }
